4202df04919b9e445fd823313ca0601bc4a8c3de
shobhit <shobjobs@gmail.com>
2020-07-15 13:06:26 +0530

    updated

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 #---------------------------------------
-# LCD Messages 
+# Flow Meter & Relay Pins 
 #---------------------------------------
 
+flo=11
+sol=16
+ 
+LCD_ADDRESS=0x27
+
+#---------------------------------------
+# LCD Messages 
+#---------------------------------------
 
 lcd_beer_name1    = "     Asahi $45      "
 lcd_tout_message  = "     Time is up     "
@@ -9,11 +17,15 @@ lcd_pour_message  = "    Pour complete   "
 lcd_ctap_message  = "  Please close tap  "
 lcd_vout_message  = "    Enjoy Drink     "
 
+#lcd_strt_message  = "   Please tap card  "
+lcd_strt_message  = "  Not in service   "
+lcd_left_message  = "  Time left to pour "
 
 lcd_left_blank_message   = "         "
 lcd_right_blank_message  = "         "
 
 lcd_count_blank_message  = "         "  
+
 
 #---------------------------------------
 # Log Messages

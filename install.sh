601eb783e475b016d63677fc2b8871ddc38c12f8
admin <shobjobs@gmail.com>
2020-07-18 07:26:34 +0100

    auto bootup

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-echo "Copy the Bunker Service....."
+echo "installing the essentials"
+sudo apt-get -y install vim-gtk
+echo "install the guizero"
+sudo pip install guizero
+echo "Copy the Bunker Service.....to bootmode"
+sudo cp app.desktop /etc/xdg/autostart/app.desktop
+echo "Reboot the System"
+sudo reboot

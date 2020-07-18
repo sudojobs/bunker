925144d17931e4af6fc601df7bd83ba968bca3e2
sudojobs <shobjobs@gmail.com>
2020-07-18 18:47:24 +0800

    boot

--- install.sh
+++ install.sh
@@ -4,5 +4,9 @@ echo "install the guizero"
 sudo pip install guizero
 echo "Copy the Bunker Service.....to bootmode"
 sudo cp app.desktop /etc/xdg/autostart/app.desktop
+echo "copy config.txt"
+sudo cp /boot/config.txt confit.txt.org
+diff config.txt config.txt.org | more
+sudo cp config.txt /boot/config.txt
 echo "Reboot the System"
 sudo reboot

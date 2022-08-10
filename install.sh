d092811ffe260d3868c44fefd2889488153c5978
sudojobs <shobjobs@gmail.com>
2022-08-10 12:00:50 +0530

    Update install.sh
    Updated with latest code and fixes

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 echo "installing the essentials"
+sudo apt-get update
 sudo apt-get -y install vim-gtk
-echo "install the guizero"
-sudo pip install guizero
+echo "install the xterm"
+sudo sudo apt-get install xterm -y
 echo "Copy the Bunker Service.....to bootmode"
-sudo cp app.desktop /etc/xdg/autostart/app.desktop
+mkdir /home/pi/.config/autostart
+sudo cp app.desktop /home/pi/.config/autostart
 echo "copy config.txt"
 sudo cp /boot/config.txt confit.txt.org
 diff config.txt config.txt.org | more
